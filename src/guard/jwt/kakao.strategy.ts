311246f47499fe033cc6a3f0d7f972470c3541aa
tech03-kwon <tech03@doubledlab.co.kr>
2022-10-21 15:06:10 +0900

    #8 카카오 로그인 유저정보 조회

--- src/guard/jwt/kakao.strategy.ts
+++ src/guard/jwt/kakao.strategy.ts
@@ -1,32 +1,36 @@
-import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-kakao';
 import { UserKakaoDto } from 'src/modules/account/auth/dto/user.kakao.dto';
-import * as config from 'config';
 
-const kakaoConfig = config.get('kakao');
-
+@Injectable()
 export class KakaoStrategy extends PassportStrategy(Strategy, 'kakao') {
   constructor() {
     super({
-      clientID: kakaoConfig.clientID,
-      callbackURL: kakaoConfig.callbackURL,
-      //   callbackURL: 'http://localhost:3000/auth/kakao/redirect',
+      clientID: '214f882001474304a397de3fa79c9de0',
+      callbackURL: 'http://localhost:3000/auth/kakao',
+      // clientID: process.env.REST_APT_KEY,
+      // callbackURL: process.env.REDIRECT_URI,
     });
   }
-
-  async validate(snsToken, refreshToken, profile, done) {
+  async validate(accessToken, refreshToken, profile, done): Promise<any> {
     const profileJson = profile._json;
     const kakao_account = profileJson.kakao_account;
+    const provider = profile.provider;
     const payload: UserKakaoDto = {
       name: kakao_account.profile.nickname,
-      snsId: profileJson.id,
+      // snsId: profileJson.id,
       email:
         kakao_account.has_email && !kakao_account.email_needs_agreement
           ? kakao_account.email
           : null,
-      snsToken,
+      birth: kakao_account.email,
+      gender: kakao_account.gender,
+      snsId: kakao_account.email,
+      snsType: '01',
+      accessToken,
     };
+    console.log('카카오 스토리지', kakao_account);
     done(null, payload);
   }
 }
