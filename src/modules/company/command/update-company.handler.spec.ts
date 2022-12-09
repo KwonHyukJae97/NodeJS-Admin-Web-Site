7d2780559fdf2c4ba9516446ad87695bb84a3e85
tech04-yang <tech04@doubledlab.co.kr>
2022-12-09 13:26:40 +0900

    #25(2) 회원사 정보 관련 테스트(성공 처리 추가/테스트명 변경/불필요한 주석 삭제)

--- src/modules/company/command/update-company.handler.spec.ts
+++ src/modules/company/command/update-company.handler.spec.ts
@@ -7,13 +7,11 @@ import { UpdateCompanyHandler } from './update-company.handler';
 import { Company } from '../entities/company.entity';
 import { UpdateCompanyCommand } from './update-company.command';
 
-// Repository에서 사용되는 함수 복제
 const mockRepository = () => ({
   save: jest.fn(),
   findOneBy: jest.fn(),
 });
 
-// MockRepository 타입 정의
 type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 
 describe('UpdateCompany', () => {
@@ -44,30 +42,30 @@ describe('UpdateCompany', () => {
   });
 
   describe('회원사 정보 정상 수정 여부', () => {
-    it('수정 성공', async () => {
-      // Given
-      const companyId = { companyId: 1 };
-
-      const newCompanyInfo = {
+    it('회원사 정보 수정 성공', async () => {
+      const companyInfo = {
         companyName: '회원사',
         businessNumber: '123-45-67890',
         companyId: 1,
       };
 
-      // 반환값 설정 (mockResolvedValue = 비동기 반환값 / mockReturnValue = 일반 반환값 반환 시 사용)
-      companyRepository.findOneBy.mockResolvedValue(companyId);
+      const newCompanyInfo = {
+        companyName: '회원사명 수정',
+        businessNumber: '123-45-67890',
+        companyId: 1,
+      };
+
+      companyRepository.findOneBy.mockResolvedValue(companyInfo);
       companyRepository.save.mockResolvedValue(newCompanyInfo);
 
-      // When
       const result = await updateCompanyHandler.execute(
         new UpdateCompanyCommand(
           newCompanyInfo.companyName,
           newCompanyInfo.businessNumber,
-          companyId.companyId,
+          newCompanyInfo.companyId,
         ),
       );
 
-      // Then
       expect(result).toEqual(newCompanyInfo);
     });
   });
