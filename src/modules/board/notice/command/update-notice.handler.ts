b757d002d74a9ca8285a2586644dd12f2f197486
tech04-yang <tech04@doubledlab.co.kr>
2022-12-23 14:29:10 +0900

    #25(2) 공지사항 트랜잭션 처리

--- src/modules/board/notice/command/update-notice.handler.ts
+++ src/modules/board/notice/command/update-notice.handler.ts
@@ -3,7 +3,7 @@ import { CommandHandler, EventBus, ICommandHandler } from '@nestjs/cqrs';
 import { UpdateNoticeCommand } from './update-notice.command';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Notice } from '../entities/notice';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { Board } from '../../entities/board';
 import { FilesUpdateEvent } from '../../../file/event/files-update-event';
 import { BoardFileDb } from '../../board-file-db';
@@ -26,6 +26,7 @@ export class UpdateNoticeHandler implements ICommandHandler<UpdateNoticeCommand>
     @Inject('noticeFile') private boardFileDb: BoardFileDb,
     @Inject(ConvertException) private convertException: ConvertException,
     private eventBus: EventBus,
+    private dataSource: DataSource,
   ) {}
 
   /**
@@ -40,6 +41,10 @@ export class UpdateNoticeHandler implements ICommandHandler<UpdateNoticeCommand>
     if (role !== '본사 관리자' && role !== '회원사 관리자') {
       throw new BadRequestException('본사 및 회원사 관리자만 접근 가능합니다.');
     }
+
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
 
     const notice = await this.noticeRepository.findOneBy({ noticeId });
 
@@ -61,42 +66,42 @@ export class UpdateNoticeHandler implements ICommandHandler<UpdateNoticeCommand>
     board.content = content;
 
     try {
-      await this.boardRepository.save(board);
-    } catch (err) {
-      return this.convertException.badRequestError('게시글 정보에', 400);
-    }
+      await queryRunner.manager.getRepository(Board).save(board);
 
-    notice.isTop = isTop;
-    notice.noticeGrant = noticeGrant;
-    notice.board = board;
+      notice.isTop = isTop;
+      notice.noticeGrant = noticeGrant;
+      notice.board = board;
 
-    try {
-      await this.noticeRepository.save(notice);
-    } catch (err) {
-      return this.convertException.badRequestError('공지사항 정보에', 400);
-    }
+      await queryRunner.manager.getRepository(Notice).save(notice);
 
-    const boardFiles = await this.fileRepository.findBy({ boardId: board.boardId });
+      const boardFiles = await this.fileRepository.findBy({ boardId: board.boardId });
 
-    if (files.length === 0) {
-      // 기존 파일만 존재하면 '삭제' 이벤트 처리
-      if (boardFiles.length !== 0) {
-        this.eventBus.publish(new FilesDeleteEvent(board.boardId, this.boardFileDb));
-      }
-    } else {
-      // 신규 파일만 존재하면 '등록' 이벤트 처리
-      if (boardFiles.length === 0) {
-        this.eventBus.publish(
-          new FilesCreateEvent(board.boardId, FileType.NOTICE, files, this.boardFileDb),
-        );
-        // 신규 파일 & 기존 파일 모두 존재하면 '수정' 이벤트 처리
+      if (files.length === 0) {
+        // 기존 파일만 존재하면 '삭제' 이벤트 처리
+        if (boardFiles.length !== 0) {
+          this.eventBus.publish(new FilesDeleteEvent(board.boardId, this.boardFileDb));
+        }
       } else {
-        this.eventBus.publish(
-          new FilesUpdateEvent(board.boardId, FileType.NOTICE, files, this.boardFileDb),
-        );
+        // 신규 파일만 존재하면 '등록' 이벤트 처리
+        if (boardFiles.length === 0) {
+          this.eventBus.publish(
+            new FilesCreateEvent(board.boardId, FileType.NOTICE, files, this.boardFileDb),
+          );
+          // 신규 파일 & 기존 파일 모두 존재하면 '수정' 이벤트 처리
+        } else {
+          this.eventBus.publish(
+            new FilesUpdateEvent(board.boardId, FileType.NOTICE, files, this.boardFileDb),
+          );
+        }
       }
-    }
+      await queryRunner.commitTransaction();
 
-    return notice;
+      return notice;
+    } catch (err) {
+      await queryRunner.rollbackTransaction();
+      return this.convertException.badInput('공지사항 정보에', 400);
+    } finally {
+      await queryRunner.release();
+    }
   }
 }
